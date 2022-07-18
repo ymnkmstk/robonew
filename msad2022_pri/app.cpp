cfc62d880f01691593b5cae56f69a5e8baf21a71
ai-terahara <dnnna_73py@yahoo.co.jp>
2022-07-18 13:31:51 +0900

    speed変えました

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -614,7 +614,7 @@ void main_task(intptr_t unused) {
     */ 
                 .composite<BrainTree::ParallelSequence>(1,2)
                    .leaf<IsTimeEarned>(1300000)
-                   .leaf<TraceLine>(SPEED_NORM, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)
+                   .leaf<TraceLine>(60, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)
                 .end()
     /*
     first turn right
